f20be7f9dd55a838a4bae39b240bd46727162968
Remleh <zerepzelletleicamremleh@hotmail.com>
2024-12-02 23:53:49 -0600

    primer commit

---
+++ biblioteca/apli/home/home_view.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+def home_views(request):
+    template_name = 'index.html'
+    return render (request, template_name)
---
+++ biblioteca/apli/login/login_view.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+def login_views(request):
+    template_name = 'login.html'
+    return render (request, template_name)
